2dd808baf0ef62fd1a241186a2db6d86b9cad5c8
Vitto <vitaliy.nykonchuk@gmail.com>
2023-09-18 01:11:29 +0300

    Initial commit

---
+++ src/main/java/user/User.java
@@ -0,0 +1,46 @@
+package user;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public final class User {
+    static final String HOST = "127.0.0.1";
+    static final int PORT = 8822;
+
+    public static void main(String[] args) throws Exception {
+
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new StringDecoder());
+                            p.addLast(new StringEncoder());
+                            p.addLast(new UserHandler());
+
+                        }
+                    });
+
+            ChannelFuture f = b.connect(HOST, PORT).sync();
+
+            String input = "User";
+            Channel channel = f.sync().channel();
+            channel.writeAndFlush(input);
+            channel.flush();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
